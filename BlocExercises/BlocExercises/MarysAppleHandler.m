1052db8e0994ef4a63cd01f4fcd8dafa572c2a51
Coby West <cobyw09@gmail.com>
2015-03-11 12:20:21 -0500

    completed if, else, ternary

--- BlocExercises/BlocExercises/MarysAppleHandler.m
+++ BlocExercises/BlocExercises/MarysAppleHandler.m
@@ -11,10 +11,28 @@
 @implementation MarysAppleHandler
 
 - (NSString *) itemMaryCanPurchaseForDollars:(NSInteger)dollars {
-    NSString *itemToReturn;
-
-    /* WORK HERE */
     
+    NSString *itemToReturn;
+if (dollars < 5) //if she has less than 5 dollars
+{
+    itemToReturn = @"get out of my store";
+}
+else if (dollars < 6) //if she has less than 6 but more than 5 dollars
+{
+    itemToReturn = @"have some gum";
+}
+else if (dollars <1000) //between 6+ and 1000-
+{
+    itemToReturn = @"have an apple";
+}
+else if (dollars <1000000000) //she has between 1000 and a hundred million
+{
+    itemToReturn = @"have an Apple computer";
+}
+else //she has at or over a hundred million dollars
+{
+    itemToReturn =@"have The Big Apple";
+}
     NSLog(@"For $%ld, Mary can: %@", (long)dollars, itemToReturn);
     return itemToReturn;
 }
