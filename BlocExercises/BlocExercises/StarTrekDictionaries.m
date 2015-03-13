d698700d01fcb385f7dd983e3f4847958c2eab00
Coby West <cobyw09@gmail.com>
2015-03-13 13:17:01 -0500

    Started on Dictionaries

--- BlocExercises/BlocExercises/StarTrekDictionaries.m
+++ BlocExercises/BlocExercises/StarTrekDictionaries.m
@@ -11,18 +11,38 @@
 @implementation StarTrekDictionaries
 
 - (NSString *)favoriteDrinkForStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
-    /* WORK HERE */
-    return @"";
+    //takes in the favorite drink
+    id favoriteDrink = characterDictionary[@"favorite drink"];
+    //makes sure it exists and is a string
+    if (favoriteDrink != nil && [favoriteDrink isKindOfClass:[NSString class]])
+        return favoriteDrink;
+    //if not returns nil
+    else
+        return nil;
 }
 
+/*NOT CURRENTLY FUNCTIONAL BELOW HERE*/
+
 - (NSArray *)arrayOfFavoriteDrinksForStarTrekCharacters:(NSArray *)charactersArray {
-    /* WORK HERE */
-    return @[];
+    //creates a mutable array to store the drinks to be returned
+    NSMutableArray *favoriteDrinks;
+    //loop through the passed array getting the favorite drinks using the favoritedrink method from above
+    for (NSInteger x = 0; x < charactersArray.count; x++)
+    {
+        favoriteDrinks[x]=[self favoriteDrinkForStarTrekCharacterDictionary:charactersArray[x]];
+    }
+
+    //returns the array
+    return favoriteDrinks;
 }
 
 - (NSDictionary *)dictionaryWithQuoteAddedToStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
-    /* WORK HERE */
-    return @{};
+    //create a mutable dictionary to be returned
+    NSMutableDictionary *starTrekWithQuotes;
+    //loops throught and copies the old dictionary to the new
+    //adds quotes
+    //returns new
+    return starTrekWithQuotes;
 }
 
 @end
