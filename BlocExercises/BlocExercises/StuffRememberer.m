462b75b797ae1b9bebbb71494efe3d870775d76b
Coby West <cobyw09@gmail.com>
2015-03-17 19:04:59 -0500

    Commented on properties and scope

--- BlocExercises/BlocExercises/StuffRememberer.m
+++ BlocExercises/BlocExercises/StuffRememberer.m
@@ -7,15 +7,15 @@
 //
 
 #import "StuffRememberer.h"
-
 @implementation StuffRememberer
-
 - (void) rememberThisArrayForLater:(NSMutableArray *)arrayToRemember {
-    /* WORK HERE */
-}
+    //makes a strong reference to arrayToRemember so it can't be deallocated???
+    NSArray *rememberer = arrayToRemember;
+    /*@property (nonatomic, strong) NSMutableArray *Remembered;*/
+};
 
 - (void) copyThisArrayForLater:(NSMutableArray *)arrayToCopy {
-    /* WORK HERE */
+    //makes a copy of the array as it currently is and stores it for later?
 }
 
 - (void) rememberThisFloatForLater:(CGFloat)floatToRemember {
